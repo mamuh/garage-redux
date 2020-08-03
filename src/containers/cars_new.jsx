1fa67b6c2e1393574c69f92923c8df9e969ee25d
Mariana Caramori <musardo@mail.com>
2020-08-03 16:42:14 -0300

    rendering field

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -3,16 +3,25 @@ import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import { addCar } from '../actions';
 
-const CarsNew = () => {
-
+class CarsNew extends Component {
+  renderField(field) {
     return (
-      <div>
-       <form>
-         <input type="text" name="brand" />
-       </form>
+      <div className="form-group"> <label>{field.label}</label>
+        <input className="form-control" type={field.type} {...field.input}/>
       </div>
     )
+  }
+
+  render() {
+    return (
+      <form>
+        <Field label="Title" name="title" type="text" component={this.renderField} />
+        <button type="submit">Create Post</button>
+      </form>
+    )
+  }
+
 
 }
 
-export default reduxForm({ form: 'newCarForm' })( connect(null, { addCar })(CarsNew));
+export default reduxForm({ form: 'newCarForm' })(connect(null, { addCar })(CarsNew));
